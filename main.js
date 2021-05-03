2e410084644b6d471743ab41629b1f05d29f00ac
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-05-03 21:38:49 +0200

    [FEATURE] counting kalories function ready

--- main.js
+++ main.js
@@ -4,8 +4,33 @@ a jako hodnotu do něj dej číslo, představující kalorickou
 hodnotu potraviny na obrázku (číslo si klidně vymysli).
 
 Zařiď, aby se při kliknutí na obrázek volala funkce,
-která vypíše kalorickou hodnotu potraviny.
+která vypíše kalorickou hodnotu potraviny. */
 
-BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
+/*BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+let obrazky = document.querySelectorAll('img');
+let snedenoKalorii = 0;
+let snedeneObrazkyDiv = document.querySelector('#snedeno');
+
+obrazky.forEach((ovoce) => {
+    ovoce.addEventListener('click', ukazKalorie);
+}) 
+
+function ukazKalorie(udalost) {
+    
+    let obrazek = udalost.target;
+    let kalorie = obrazek.dataset.kalorie;
+
+    document.querySelector('#odpoved').innerHTML = "Právě jsi snědl " + kalorie + " kalorií.";
+
+    snedenoKalorii = parseInt(snedenoKalorii) + parseInt(kalorie);
+
+    document.querySelector('#vysledek').innerHTML = "Celkově jsi dnes snědl " + snedenoKalorii + " kalorií.";
+
+    obrazek.removeEventListener('click', ukazKalorie);
+    snedeneObrazkyDiv.appendChild(obrazek);
+
+}
+
